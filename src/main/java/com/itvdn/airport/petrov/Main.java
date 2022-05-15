78b54d42556e711f4ba0fe2fa9c55898fd4dcd64
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2022-05-15 22:52:04 +0300

    in progress

--- src/main/java/com/itvdn/airport/petrov/Main.java
+++ src/main/java/com/itvdn/airport/petrov/Main.java
@@ -1,17 +1,7 @@
 package com.itvdn.airport.petrov;
 
-import com.itvdn.airport.petrov.configuration.database.DataBase;
-import com.itvdn.airport.petrov.dao.AirlineDAO;
-import com.itvdn.airport.petrov.dao.DAOFactory;
-import com.itvdn.airport.petrov.dao.impl.DAOFactoryImpl;
-import com.itvdn.airport.petrov.entity.Airline;
-import com.itvdn.airport.petrov.repository.PassengerRepository;
-import com.itvdn.airport.petrov.repository.PlaneRepository;
-import com.itvdn.airport.petrov.repository.impl.RepositoryFactoryImpl;
-
 public class Main {
     public static void main(String[] args) {
-        PassengerRepository passengerRepository = new RepositoryFactoryImpl().getPassengerRepository();
-        System.out.println(passengerRepository.getAllArrived().size());
+
     }
 }
